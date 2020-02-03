c1a40f7c2e88261cb5d3200987dc16a7686b15dc
pch720 <qkrcksgml56@naver.com>
2020-02-03 14:10:08 +0900

    update 2020-02-03

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -18,36 +18,26 @@ public class MemberController {
         return "/index";
     }
 
-    // 회원가입 페이지
-    @GetMapping("/user/signup")
-    public String dispSignup() {
-        return "/signup";
-    }
+
 
     // 회원가입 처리
     @PostMapping("/user/signup")
     public String execSignup(MemberDto memberDto) {
         memberService.joinUser(memberDto);
 
-        return "redirect:/user/login";
-    }
-
-    // 로그인 페이지
-    @GetMapping("/user/login")
-    public String dispLogin() {
-        return "/login";
+        return "redirect:/";
     }
 
     // 로그인 결과 페이지
     @GetMapping("/user/login/result")
     public String dispLoginResult() {
-        return "/loginSuccess";
+        return "/";
     }
 
     // 로그아웃 결과 페이지
     @GetMapping("/user/logout/result")
     public String dispLogout() {
-        return "/logout";
+        return "/";
     }
 
     // 접근 거부 페이지
